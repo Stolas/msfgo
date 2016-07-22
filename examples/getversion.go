07128590b7d8e678b9a927ef6929a7bb72b2118e
Robin Vossen <robin@codeinject.org>
2016-07-23 00:19:07 +0200

    Include First Example

---
+++ examples/getversion.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/AtraxResearch/msfgo"
+)
+
+func main() {
+	var conn msf.MetasploitConnection
+	conn.Url = "192.168.178.36"
+	conn.Port = 55554
+
+	session, err := msf.NewSession(conn, "msf", "msf")
+	if err != nil {
+		panic(err)
+	}
+
+	msfver, rubyver, err := msf.GetVersion(*session)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Metasploit Version:", msfver)
+	fmt.Println("Ruby Version:", rubyver)
+}
